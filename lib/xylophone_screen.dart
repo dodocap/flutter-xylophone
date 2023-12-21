c5e1cc5f7632186bccefe7c8c8730acb2b205516
dodocap <dodocap@naver.com>
2023-12-21 16:20:09 +0900

    feat: soundpool process handling
    1. 사운드 로드 완료까지 대기 처리
    2. 각 버튼에 사운드 연결

--- lib/xylophone_screen.dart
+++ lib/xylophone_screen.dart
@@ -16,6 +16,8 @@ class _XylophoneScreenState extends State<XylophoneScreen> {
 
   final List<int> _soundIdList = [];
 
+  bool _isLoading = true;
+
   @override
   void initState() {
     _initSoundPool();
@@ -33,7 +35,7 @@ class _XylophoneScreenState extends State<XylophoneScreen> {
     soundId = await rootBundle.load('assets/mi.wav').then((soundData) => _soundpool.load(soundData));
     _soundIdList.add(soundId);
 
-    soundId = await rootBundle.load('assets/pa.wav').then((soundData) => _soundpool.load(soundData));
+    soundId = await rootBundle.load('assets/fa.wav').then((soundData) => _soundpool.load(soundData));
     _soundIdList.add(soundId);
 
     soundId = await rootBundle.load('assets/sol.wav').then((soundData) => _soundpool.load(soundData));
@@ -47,6 +49,10 @@ class _XylophoneScreenState extends State<XylophoneScreen> {
 
     soundId = await rootBundle.load('assets/do2.wav').then((soundData) => _soundpool.load(soundData));
     _soundIdList.add(soundId);
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
@@ -54,55 +60,62 @@ class _XylophoneScreenState extends State<XylophoneScreen> {
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
 
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: keyboard('도', const Color.fromARGB(255, 255, 0, 0)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: keyboard('레', const Color.fromARGB(255, 255, 165, 0)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32.0),
-            child: keyboard('미', const Color.fromARGB(255, 255, 255, 0)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40.0),
-            child: keyboard('파', const Color.fromARGB(255, 0, 128, 0)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 48.0),
-            child: keyboard('솔', const Color.fromARGB(255, 0, 255, 0)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 56.0),
-            child: keyboard('라', const Color.fromARGB(255, 0, 0, 255)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 64.0),
-            child: keyboard('시', const Color.fromARGB(255, 0, 0, 128)),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 72.0),
-            child: keyboard('도', const Color.fromARGB(255, 128, 0, 128)),
-          ),
-        ],
-      ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: keyboard('도', const Color.fromARGB(255, 255, 0, 0), _soundIdList[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: keyboard('레', const Color.fromARGB(255, 255, 165, 0), _soundIdList[1]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: keyboard('미', const Color.fromARGB(255, 255, 255, 0), _soundIdList[2]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40.0),
+                  child: keyboard('파', const Color.fromARGB(255, 0, 128, 0), _soundIdList[3]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48.0),
+                  child: keyboard('솔', const Color.fromARGB(255, 0, 255, 0), _soundIdList[4]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 56.0),
+                  child: keyboard('라', const Color.fromARGB(255, 0, 0, 255), _soundIdList[5]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 64.0),
+                  child: keyboard('시', const Color.fromARGB(255, 0, 0, 128), _soundIdList[6]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 72.0),
+                  child: keyboard('도', const Color.fromARGB(255, 128, 0, 128), _soundIdList[7]),
+                ),
+              ],
+            ),
     );
   }
 
-  Widget keyboard(String text, Color color) {
-    return Container(
-      width: 50,
-      height: double.infinity,
-      color: color,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(color: Colors.white),
+  Widget keyboard(String text, Color color, int soundId) {
+    return InkWell(
+      onTap: () {
+        _soundpool.play(soundId);
+      },
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
